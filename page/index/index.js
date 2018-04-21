bf3248265cf5068d109104d04a5eb53cfb043ee4
wanghaixiao <wanghaixiao@koopoo.com.cn>
2018-04-21 20:50:31 +0800

    init

---
+++ page/index/index.js
@@ -0,0 +1,93 @@
+// page/index/index.js
+let utils = require('../../utils/util.js')
+let app = getApp()
+let userCalendar
+const date_arr = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"]
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nowDate:'获取中',
+    prev_times:'获取中'
+  },
+  onLoad: function () {
+    let Interval = setInterval(this.changeTime,1000)
+  },
+  onShow(){
+    this.getPrev()
+  },
+  getPrev(){
+    userCalendar = wx.getStorageSync('userCalendar')
+    if (userCalendar == undefined || userCalendar == '') {
+      userCalendar = []
+    } else {
+      userCalendar = JSON.parse(userCalendar)
+    }
+    if (userCalendar.length > 0) {
+      let prev = userCalendar[userCalendar.length - 1]
+      this.data.prev_times = prev.calendarAf == undefined ? prev.calendarM0 : prev.calendarAf
+      if (this.data.prev_times == undefined) {
+        this.data.prev_times = '暂无'
+      }
+      this.setData({
+        prev_times: this.data.prev_times
+      })
+    } else {
+      this.setData({
+        prev_times: '暂无'
+      })
+    }
+  },
+  changeTime(){
+    this.setData({
+      nowDate: utils.formatTime(new Date())
+    })
+  },
+  calendar(){
+    //签到 
+    let _date = new Date()
+    let nowTimes = utils.formatDate(_date)
+    
+    if (_date.getHours()<12){
+      // 小于十二小时为上午签到
+      // 判断今天是否已经签到==>已签到，退出
+      if (userCalendar.length > 0 && userCalendar[userCalendar.length - 1].date == nowTimes){
+        // 已签到
+        wx.showToast({
+          title: '请勿重复签到',
+          icon: 'none',
+          duration: 2000
+        })
+        return
+      }
+      userCalendar.push({
+        date: nowTimes,
+        week: date_arr[_date.getDay()],
+        calendarM0: utils.formatTime(_date)
+      })
+      wx.showToast({
+        title: '签到成功',
+        icon: 'none',
+        duration: 2000
+      })
+    }else{
+      if (userCalendar.length > 0 && userCalendar[userCalendar.length - 1].date == nowTimes){
+        userCalendar[userCalendar.length - 1].calendarAf = utils.formatTime(_date)
+      }else{
+        userCalendar.push({
+          date: nowTimes,
+          week: date_arr[_date.getDay()],
+          calendarAf: utils.formatTime(_date)
+        })
+      }
+      wx.showToast({
+        title: '签到成功',
+        icon: 'none',
+        duration: 2000
+      })
+    }
+    wx.setStorageSync('userCalendar', JSON.stringify(userCalendar))
+    this.getPrev()
+  }
+})
---
+++ page/record/record.js
@@ -0,0 +1,42 @@
+// page/record/record.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userCalendar:[]
+  },
+  onShow: function (options) {
+    this.getCalendar()
+  },
+  getCalendar(){
+    let userCalendar = wx.getStorageSync('userCalendar')
+    if (userCalendar == undefined || userCalendar=='') {
+      userCalendar = []
+    } else {
+      userCalendar = JSON.parse(userCalendar)
+    }
+    this.setData({
+      userCalendar: userCalendar
+    })
+    console.log(this.data.userCalendar)
+  },
+  del(){
+    let self = this
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除签到记录吗',
+      success: function (res) {
+        if (res.confirm) {
+          wx.clearStorageSync('userCalendar')
+          wx.showToast({
+            title: '已清除',
+            icon: 'none',
+            duration: 2000
+          })
+          self.getCalendar()
+        }
+      }
+    })
+  }
+})
