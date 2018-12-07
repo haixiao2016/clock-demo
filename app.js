dd9c184faaf519c3aa6f9d558b7f50dcc9528c86
haixiao2016 <haixiao@yeah.net>
2018-12-07 10:15:31 +0800

    refactor: 添加热更新

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 //app.js
 App({
-  onLaunch: function () {
-
+  onLaunch: function() {
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function(){})
+    updateManager.onUpdateReady(function() {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，立即享用新版吧~',
+        showCancel: false,
+        success: function(res) {
+          if (res.confirm) {
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
   },
   globalData: {
     userCalendar: []
