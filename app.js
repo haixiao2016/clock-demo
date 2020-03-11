4e5cffd7c4860663a1e769e87c5d23b0667bc0d9
haixiao2016 <haixiao@yeah.net>
2020-03-11 14:29:57 +0800

    feat: 更改接口

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ App({
     const updateManager = wx.getUpdateManager()
     updateManager.onCheckForUpdate(function(){})
     updateManager.onUpdateReady(function() {
-      wx.showModal({
-        title: '更新提示',
-        content: '新版本已经准备好，立即享用新版吧~',
-        showCancel: false,
-        success: function(res) {
-          if (res.confirm) {
-            updateManager.applyUpdate()
-          }
-        }
-      })
+      // wx.showModal({
+      //   title: '更新提示',
+      //   content: '新版本已经准备好，立即享用新版吧~',
+      //   showCancel: false,
+      //   success: function(res) {
+      //     if (res.confirm) {
+      //       updateManager.applyUpdate()
+      //     }
+      //   }
+      // })
     })
   },
   globalData: {
--- page/record/record.js
+++ page/record/record.js
@@ -1,129 +1,148 @@
 // page/record/record.js
-let utils = require('../../utils/util.js')
-let t_index = -1
+let utils = require("../../utils/util.js");
+let t_index = -1;
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    userCalendar:undefined,
+    userCalendar: undefined,
     monArr: [],
     lawArr: []
   },
   onShow() {
-    this.getLawDay()
+    let LawDay = wx.getStorageSync("LawDay");
+    if (LawDay == undefined || LawDay == "") {
+      LawDay = {};
+    } else {
+      LawDay = JSON.parse(LawDay);
+    }
+    this.setData(
+      {
+        monArr: LawDay.monArr || [],
+        lawArr: LawDay.lawArr || []
+      },
+      () => {
+        this.getCalendar();
+      }
+    );
   },
-  getLawDay(){
-    const _this = this
-    if(this.data.lawArr.length === 0){
-      wx.showLoading({
-        title: '获取中',
-      })
-      wx.request({
-        url: 'https://easy-mock.com/mock/5e6499658e711c50e7d330cf/clock/reqGetLawDay',
-        success(res) {
-          const data = res.data || {}
+  onLoad() {
+    this.getPrevRefresh();
+  },
+  getPrevRefresh() {
+    const refreshId = wx.getStorageSync("refreshId");
+    const _this = this;
+    wx.request({
+      url:
+        "https://www.fastmock.site/mock/8d9c7887911b5fe831371bef61dee0c2/clock/reqGetLawDay",
+      success(res) {
+        const data = res.data || {};
+        if (refreshId !== data.refreshId) {
+          wx.setStorageSync('refreshId', data.refreshId);
+          wx.setStorageSync('LawDay', JSON.stringify({
+            monArr: data.monArr,
+            lawArr: data.lawArr,
+          }));
           _this.setData({
             monArr: data.monArr,
             lawArr: data.lawArr
-          })
-        },
-        complete(){
-          _this.getCalendar()
-        }
-      })
-    } else {
-      _this.getCalendar()
-    }
-  },
-  getCalendar(){
-    const { monArr,lawArr } = this.data
-    let userCalendar = wx.getStorageSync('userCalendar')
-    if (userCalendar == undefined || userCalendar=='') {
-      userCalendar = []
-    } else {
-      userCalendar = JSON.parse(userCalendar)
-    }
-    userCalendar.map((item)=>{
-      item.low = utils.gusLow(item.date,lawArr)
-      item.mon = utils.monLow(item.date,monArr)
-    })
-    this.setData({
-      userCalendar
-    })
-    wx.hideLoading()
-    this.refresh()
-  },
-  del(){
-    let self = this
-    wx.showModal({
-      title: '提示',
-      content: '确定要删除签到记录吗',
-      success: function (res) {
-        if (res.confirm) {
-          wx.clearStorageSync('userCalendar')
-          wx.showToast({
-            title: '已清除',
-            icon: 'none',
-            duration: 2000
-          })
-          self.getCalendar()
+          },()=> _this.getCalendar());
         }
       }
-    })
+    });
+  },
+  getCalendar() {
+    const { monArr, lawArr } = this.data;
+    let userCalendar = wx.getStorageSync("userCalendar");
+    if (userCalendar == undefined || userCalendar == "") {
+      userCalendar = [];
+    } else {
+      userCalendar = JSON.parse(userCalendar);
+    }
+    userCalendar.map(item => {
+      item.low = utils.gusLow(item.date, lawArr);
+      item.mon = utils.monLow(item.date, monArr);
+    });
+    this.setData({
+      userCalendar
+    });
+    wx.hideLoading();
+    this.refresh();
+  },
+  del() {
+    let self = this;
+    wx.showModal({
+      title: "提示",
+      content: "确定要删除签到记录吗",
+      success: function(res) {
+        if (res.confirm) {
+          wx.clearStorageSync("userCalendar");
+          wx.showToast({
+            title: "已清除",
+            icon: "none",
+            duration: 2000
+          });
+          self.getCalendar();
+        }
+      }
+    });
   },
   refresh(i) {
     if (this.data.userCalendar && this.data.userCalendar.length > 0) {
       this.data.userCalendar.map((item, index) => {
         if (i !== index) {
-          this.selectComponent(`#slip${index}`) && this.selectComponent(`#slip${index}`).refresh()
+          this.selectComponent(`#slip${index}`) &&
+            this.selectComponent(`#slip${index}`).refresh();
           // 调用组件内部分`refresh`方法
           // 只改变组件显示状态，不支持动态改变组件相关信息，如按钮组的宽度等信息
           // 如果需要改变的话，需要重新渲染，请调用`init`方法.
         }
-      })
+      });
     }
   },
   opend(e) {
-    const opend = e.currentTarget.dataset ? e.currentTarget.dataset.opend : t_index
-    if (opend === t_index){
-      t_index = -1
-      return this.refresh(-1)
+    const opend = e.currentTarget.dataset
+      ? e.currentTarget.dataset.opend
+      : t_index;
+    if (opend === t_index) {
+      t_index = -1;
+      return this.refresh(-1);
     }
-    t_index = opend
+    t_index = opend;
     // 只允许同时打开一个左划删除组件，当打开一个组件时，其他组件将关闭。不使用可以删除
-    return this.refresh(opend)
+    return this.refresh(opend);
   },
-  delBtnClick(e){
-    let userCalendar = wx.getStorageSync('userCalendar')
-    if (userCalendar == undefined || userCalendar == '') {
-      userCalendar = []
+  delBtnClick(e) {
+    let userCalendar = wx.getStorageSync("userCalendar");
+    if (userCalendar == undefined || userCalendar == "") {
+      userCalendar = [];
     } else {
-      userCalendar = JSON.parse(userCalendar)
+      userCalendar = JSON.parse(userCalendar);
     }
-    const id = Number(e.currentTarget.id)
-    const date = userCalendar[id]
-    let self = this
+    const id = Number(e.currentTarget.id);
+    const date = userCalendar[id];
+    let self = this;
     wx.showModal({
-      title: '提示',
+      title: "提示",
       content: `确定要删除${date.date}的签到记录吗`,
-      success: function (res) {
+      success: function(res) {
         if (res.confirm) {
-          setTimeout(()=>{
-            userCalendar.splice(id, 1)
-            wx.setStorageSync('userCalendar', JSON.stringify(userCalendar))
+          setTimeout(() => {
+            userCalendar.splice(id, 1);
+            wx.setStorageSync("userCalendar", JSON.stringify(userCalendar));
             wx.showToast({
-              title: '已清除',
-              icon: 'none',
+              title: "已清除",
+              icon: "none",
               duration: 2000
-            })
-            self.getCalendar()
-          },300)
+            });
+            self.getCalendar();
+          }, 300);
         }
       },
-      complete:function(){
-        self.refresh()
+      complete: function() {
+        self.refresh();
       }
-    })
+    });
   }
-})
+});
